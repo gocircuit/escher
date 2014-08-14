342f74f86b285a034f74818d8e6a18cad7d4c48f
Petar Maymounkov <petarm@gmail.com>
2014-08-14 07:47:11 -0400

    add clunk to io faculty

--- escher/main.go
+++ escher/main.go
@@ -18,13 +18,14 @@ import (
 
 	"github.com/gocircuit/escher/faculty/basic"
 	"github.com/gocircuit/escher/faculty/circuit"
+	_ "github.com/gocircuit/escher/faculty/io"
 	facultyos "github.com/gocircuit/escher/faculty/os"
 	_ "github.com/gocircuit/escher/faculty/time"
 )
 
 var (
 	flagLex  = flag.Bool("lex", false, "parse and show faculties without running")
-	flagSrc  = flag.String("src", "", "program source directory")
+	flagDir  = flag.String("dir", "", "program source directory")
 	flagName = flag.String("name", "", "execution name")
 	flagArg = flag.String("arg", "", "program arguments")
 	flagDiscover = flag.String("discover", "", "multicast UDP discovery address for circuit faculty, if needed")
@@ -34,14 +35,14 @@ func main() {
 	flag.Parse()
 	basic.Init(*flagName)
 	facultyos.Init(*flagArg)
-	if *flagSrc == "" {
+	if *flagDir == "" {
 		fatalf("source directory must be specified with -src")
 	}
 	if *flagLex {
-		fmt.Println(compile(*flagSrc).Print("", "   "))
+		fmt.Println(compile(*flagDir).Print("", "   "))
 	} else {
 		loadCircuitFaculty(*flagName, *flagDiscover)
-		think.Space(compile(*flagSrc)).Materialize("main")
+		think.Space(compile(*flagDir)).Materialize("main")
 		select{} // wait forever
 	}
 }
---
+++ faculty/io/io.go
@@ -0,0 +1,44 @@
+// Written in 2014 by Petar Maymounkov.
+//
+// It helps future understanding of past knowledge to save
+// this notice, so peers of other times and backgrounds can
+// see history clearly.
+
+// Package io provides gates for manipulating Go's I/O types.
+package io
+
+import (
+	"io"
+	"io/ioutil"
+
+	"github.com/gocircuit/escher/think"
+	"github.com/gocircuit/escher/faculty"
+)
+
+func init() {
+	faculty.Root.AddTerminal("clunk", Clunk{})
+}
+
+// Clunk…
+type Clunk struct{}
+
+func (Clunk) Materialize() think.Reflex {
+	ioEndo, ioExo := think.NewSynapse()
+	go func() {
+		ioEndo.Focus(clunk)
+	}()
+	return think.Reflex{
+		"IO": ioExo, 
+	}
+}
+
+func clunk(v interface{}) {
+	switch t := v.(type) {
+	case io.Closer:
+		t.Close()
+	case io.Reader:
+		io.Copy(ioutil.Discard, t)
+	default:
+		panic("io.clunk sees unrecognized type")
+	}
+}

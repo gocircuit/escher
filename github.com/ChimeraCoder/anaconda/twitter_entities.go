7287686ac5de01a80a6c7598aa023996f7cb868b
Petar Maymounkov <petarm@gmail.com>
2014-08-23 16:16:05 -0400

    add ChimeraCoder/anaconda and ChimeraCoder/tokenbucket for backend of a Twitter faculty

---
+++ github.com/ChimeraCoder/anaconda/twitter_entities.go
@@ -0,0 +1,46 @@
+package anaconda
+
+type Entities struct {
+	Hashtags []struct {
+		Indices []int
+		Text    string
+	}
+	Urls []struct {
+		Indices      []int
+		Url          string
+		Display_url  string
+		Expanded_url string
+	}
+	User_mentions []struct {
+		Name        string
+		Indices     []int
+		Screen_name string
+		Id          int64
+		Id_str      string
+	}
+	Media []struct {
+		Id              int64
+		Id_str          string
+		Media_url       string
+		Media_url_https string
+		Url             string
+		Display_url     string
+		Expanded_url    string
+		Sizes           MediaSizes
+		Type            string
+		Indices         []int
+	}
+}
+
+type MediaSizes struct {
+	Medium MediaSize
+	Thumb  MediaSize
+	Small  MediaSize
+	Large  MediaSize
+}
+
+type MediaSize struct {
+	W      int
+	H      int
+	Resize string
+}

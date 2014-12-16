c3bca6dbc24370ef370144c8a98b9c84c6709db4
Petar Maymounkov <petarm@gmail.com>
2014-12-16 15:40:44 +0530

    doc

--- a/lex.go
+++ a/lex.go
@@ -36,7 +36,7 @@ func IsIdentifier(r rune) bool {
 		return true
 	case r >= '0' && r <= '9':
 		return true
-	case r == '_', r == '?', r == '@', r == '*', r == '-', r == '+', r == '$':
+	case r == '_', r == '?', r == '-', r == '+', r == '$', r == '*', r == '@':
 		return true
 	}
 	return false

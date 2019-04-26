a44a4d8b75d062c3b1054f782aae4e7b331a9f08
hoijui <hoijui.quaero@gmail.com>
2019-04-26 22:05:23 +0200

    generate.sh: also copy static resources from the handbook sources

--- generate.sh
+++ generate.sh
@@ -2,5 +2,13 @@
 # *nix script to generate the HTML handbook
 # from the sources in "github.com/gocircuit/escher/src/handbook".
 
-escher -src "$GOPATH/src/github.com/gocircuit/escher/src/" "*handbook.main"
+src_dir="$GOPATH/src/github.com/gocircuit/escher/src/"
+
+escher -src "$src_dir" "*handbook.main"
+
+cp -r "$src_dir/handbook/css" ./
+cp -r "$src_dir/handbook/img" ./
+cp -r "$src_dir/handbook/pdf" ./
+
+rm -f css/font/.gitignore
 
